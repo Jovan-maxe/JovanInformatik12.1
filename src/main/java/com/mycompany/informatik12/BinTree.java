99ee2b0fd50d3a796d8f4cf064f24204c1e72bc3
jovan.gehrke <jovan.gehrke@A202-TC-13.ad.maxe-del.de>
2025-12-05 14:16:42 +0100

    wert enthalten?

--- src/main/java/com/mycompany/informatik12/BinTree.java
+++ src/main/java/com/mycompany/informatik12/BinTree.java
@@ -9,25 +9,26 @@ package com.mycompany.informatik12;
  * @author jovan.gehrke
  */
 public class BinTree {
-      //Attribute
+    //Attribute
+
     BinTree right;
     BinTree left;
     int value;
-    
+
     //Methoden
-        // Konstruktoren
+    // Konstruktoren
     BinTree() {
         right = null;
         left = null;
     }
-    
+
     BinTree(int value) {
         this.value = value;
         right = null;
         left = null;
     }
-    
-        //Sonstige Methoden
+
+    //Sonstige Methoden
     boolean isEmpty() {
         if (value == 0 && right == null && left == null) {
             return true;
@@ -35,15 +36,15 @@ public class BinTree {
             return false;
         }
     }
-    
+
     int getItem() {
         return this.value;
     }
-        
+
     void setItem(int value) {
         this.value = value;
     }
-    
+
     boolean isLeaf() {
         if (right == null && left == null) {
             return true;
@@ -51,40 +52,98 @@ public class BinTree {
             return false;
         }
     }
-    
+
     BinTree getLeft() {
         return this.left;
     }
-    
+
+    boolean hasLeft() {
+        if (this.left != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean hasRight() {
+        if (this.right != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     void setLeft(BinTree left) {
         this.left = left;
     }
-    
+
     BinTree getRight() {
         return right;
     }
-    
+
     void setRight(BinTree right) {
         this.right = right;
     }
-    
+
     void setEmpty() {
         value = 0;
         left = null;
         right = null;
-    }   
+    }
+
+    void append(int zahl) {
+        if (value == 0) {
+            this.value = zahl;
+        }
+        if (zahl < value) {
+            if (hasLeft() == true) {
+                this.left.append(zahl);
+            } else {
+                BinTree links = new BinTree(zahl);
+                this.left = links;
+            }
+        } else if (zahl > value) {
+            if (hasRight() == true) {
+                this.right.append(zahl);
+            } else {
+                BinTree rechts = new BinTree(zahl);
+                this.left = rechts;
+            }
+        } else {
+            System.out.print("Number already exists");
+        }
+
+    }
+
+    boolean contains(int zahl2) {
+        if (value == 0) {
+            return false;
+        }
+
+        if (zahl2 < value) {
+
+            this.left.append(zahl);
+
+        }
+    }
+    else if (zahl2 > value
+
     
-    void append(int value){
-    if (value < ){
+        ) {
+            if (hasRight() == true) {
+            this.right.append(zahl);
+        } else {
+            BinTree rechts = new BinTree(zahl);
+            this.left = rechts;
+        }
+    }
+
+    
+    
+
+    else {
+
+        }
         
+    void delete(int value) {
     }
-    }
-  boolean contains(int value){
-      if(value == ){
-          
-      }
-    return false;
-  }
-  void delete(int value){
-}
-}
